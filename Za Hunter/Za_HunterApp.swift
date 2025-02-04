d136c0dcaaa8b948f3622f02491e5c9b85828a02
Aiden Baker <aidenbak9@gmail.com>
2025-02-04 09:53:58 -0600

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Aiden Baker on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Za HunterTests/Za_HunterTests.swift
@@ -0,0 +1,16 @@
+//
+//  Za_HunterTests.swift
+//  Za HunterTests
+//
+//  Created by Aiden Baker on 2/4/25.
+//
+
+import Testing
+
+struct Za_HunterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
